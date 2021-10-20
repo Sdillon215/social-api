c0bfe70c84a633a3198b92876d45f9967bcb29be
Sean <sdillon215@gmail.com>
2021-10-19 22:18:59 -0400

    user model almost there

--- models/User.js
+++ models/User.js
@@ -1,17 +1,44 @@
 const { Schema, model } = require('mongoose');
 
-const UserSchema = new Schema({
-    userName: {
-        type: String,
-        required: true,
-        trim: true,
-        unique: true
+const UserSchema = new Schema(
+    {
+        userName: {
+            type: String,
+            required: true,
+            trim: true,
+            unique: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true
+            // look up email validation
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thoughts'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
     },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-        // look up email validation
-    },
-    
-})
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    }
+);
+
+UserSchema.virtual('friendCount').get(function () {
+    return this.friends.reduce((total, user) => total + user.friends.length + 1, 0);
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
